bef9cc57ebd435a83f5bef7bd081e326d77cba54
Kazuki_Yasumoto <Kazuki_Yasumoto@Kazuki-Yasumoto-no-MacBook-Air.local>
2016-12-18 21:35:20 +0900

    complete

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,8 +2,9 @@ class Listing < ActiveRecord::Base
   belongs_to :user
   has_many :photos
   has_many :reservations
+  has_many :reviews
 
-
+  #必須項目
   validates :home_type, presence: true
   validates :pet_type, presence: true
   validates :pet_size, presence: true
@@ -12,4 +13,7 @@ class Listing < ActiveRecord::Base
   geocoded_by :address
   after_validation :geocode, :if => :address_changed?
 
+  def average_star_rate
+    reviews.count == 0 ? 0 : reviews.average(:rate).round(1)
+  end
 end
