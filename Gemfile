c5d41baee23bfd3d86ab618bb256ad3fa1d2adbe
Kazuki_Yasumoto <Kazuki_Yasumoto@Kazuki-Yasumoto-no-MacBook-Air.local>
2016-12-18 16:56:12 +0900

    Add stipe

--- Gemfile
+++ Gemfile
@@ -18,6 +18,9 @@ gem "paperclip", "~> 5.0.0"
 gem 'dropzonejs-rails'
 gem "figaro", "~> 1.1.0"
 gem 'aws-sdk', '~> 2.3'
+gem 'geocoder'
+gem 'jquery-ui-rails'
+gem 'stripe'
 
 group :production do
   gem "pg"
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,9 +1,15 @@
 class Listing < ActiveRecord::Base
   belongs_to :user
   has_many :photos
+  has_many :reservations
+
 
   validates :home_type, presence: true
   validates :pet_type, presence: true
   validates :pet_size, presence: true
   validates :breeding_years, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, :if => :address_changed?
+
 end
